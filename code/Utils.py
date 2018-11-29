86f50daa9270eae70463964514277d9f3746ab50
Peter Kong <peter@peterkong.com>
2018-11-29 00:22:11 -0500

    minor mods

--- code/Utils.py
+++ code/Utils.py
@@ -6,20 +6,24 @@ import functools as fn
 
 
 class Utils:
-  @staticmethod
-  def check_for_nulls(df):
-      nulls_dict = {}
-      for col in df.columns:
-          mask = df[col].isnull()
-          counter = 0
-          for cell in mask:
-              if cell == True:
-                  counter = counter + 1
-          nulls_dict[col] = counter
-      return nulls_dict
-          
-  @staticmethod
-  def stem_n_stop(text, spacy):
-      wordlist = nlp(text)
-      # nlp = spacy.load('en_core_web_sm')
-      # doc = nlp(u'Apple is looking at buying U.K. startup for $1 billion')
+    @staticmethod
+    def check_for_nulls(df):
+        nulls_dict = {}
+        for col in df.columns:
+            mask = df[col].isnull()
+            counter = 0
+            for cell in mask:
+                if cell == True:
+                    counter = counter + 1
+            nulls_dict[col] = counter
+        return nulls_dict
+            
+    @staticmethod
+    def stem_n_stop(text, spacy):
+        wordlist = []
+        tokens = spacy(text)
+        for token in tokens:
+            wordlist.append(token.text)
+        return wordlist
+        # nlp = spacy.load('en_core_web_sm')
+        # doc = nlp(u'Apple is looking at buying U.K. startup for $1 billion')
--- tests/test_Utils.py
+++ tests/test_Utils.py
@@ -1,12 +1,16 @@
 import unittest
-
 from Utils import Utils
 
-
+import spacy
 
 class Tester(unittest.TestCase):
     def test_vulgar(self):
         # this path param is relative to test_runner.py
 
         self.assertEqual(False, False)
+
+    def test_stem_n_stop(text):
+        nlp = spacy.load('en_core_web_sm')
+        result = Utils.stem_n_stop("quick brown apple apples", nlp)
+        print(result)
 
